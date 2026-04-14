d123d29b25a45e64e0787a893af62b0897e973dc
Petr <petr.cizek@xax.cz>
2026-04-15 00:17:50 +0200

    Test for deploy

--- lib/auth.ts
+++
@@ -1,7 +0,0 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export async function getSession(req: NextApiRequest, res: NextApiResponse) {
-    return getServerSession(req, res, authOptions);
-}
